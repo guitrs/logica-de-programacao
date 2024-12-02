c91281607583ace20db499ff06ed96db2d881dfb
guitrs <guitrs@gmail.com>
2024-12-02 20:46:20 -0300

    adicionando informações para criar código

---
+++ aula.js
@@ -0,0 +1,3 @@
+// 1. O usuário vai encostar o bilhete na catraca
+// 2. A catraca vai verificar SE trm saldo no bilhete
+// 3. SENÃO tiver saldo no bilhete, mostrar uma mensagem de "saldo insuficiente"
